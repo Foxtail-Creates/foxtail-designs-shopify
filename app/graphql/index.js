d9f35811125b8d9bb6ad42252ee97971653c4b79
klee97 <klee9700@gmail.com>
2024-07-03 10:22:03 -0700

    reoder queries

--- app/graphql/index.js
+++ app/graphql/index.js
@@ -1,21 +1,15 @@
 /**
- * Mutations
- * Further info: https://shopify.dev/concepts/graphql/queries
+ * GraphQL Mutations and Queries
  */
-export { GET_PRODUCT_BY_ID_QUERY } from "./product/getProductById";
+export { CREATE_VARIANT_QUERY } from "./queries/productVariant/createVariant";
+export { BULK_CREATE_VARIANTS_QUERY } from "./queries/productVariant/bulkCreateVariants";
 
-export { GET_SHOP_METAFIELD_BY_KEY_QUERY } from "./shop/getShopMetafieldByKey";
-/**
- * Queries
- * Further info: https://shopify.dev/concepts/graphql/mutations
- */
-export { CREATE_VARIANT_QUERY } from "./productVariant/createVariant";
-export { BULK_CREATE_VARIANTS_QUERY } from "./productVariant/bulkCreateVariants";
+export { CREATE_PRODUCT_WITH_OPTIONS_QUERY } from "./queries/product/createProductWithOptions";
+export { SET_PRODUCT_METAFIELD_QUERY } from "./queries/product/setProductMetafield";
 
-export { CREATE_PRODUCT_WITH_OPTIONS_QUERY } from "./product/createProductWithOptions";
-export { SET_PRODUCT_METAFIELD_QUERY } from "./product/setProductMetafield";
+export { GET_PRODUCT_BY_ID_QUERY } from "./queries/product/getProductById";
+export { CREATE_PRODUCT_OPTIONS_QUERY } from "./queries/productOption/createProductOptions";
+export { UPDATE_PRODUCT_OPTIONS_AND_VARIANTS_QUERY } from "./queries/productOption/updateProductOptionsAndVariants";
 
-export { CREATE_PRODUCT_OPTIONS_QUERY } from "./productOption/createProductOptions";
-export { UPDATE_PRODUCT_OPTIONS_AND_VARIANTS_QUERY } from "./productOption/updateProductOptionsAndVariants";
-
-export { SET_SHOP_METAFIELDS_QUERY } from "./shop/setShopMetafields";
+export { GET_SHOP_METAFIELD_BY_KEY_QUERY } from "./queries/shop/getShopMetafieldByKey";
+export { SET_SHOP_METAFIELDS_QUERY } from "./queries/shop/setShopMetafields";
