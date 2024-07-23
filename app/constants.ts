bde8bd92af808f1dcd12e752423dca880c2aa20f
klee97 <klee9700@gmail.com>
2024-07-23 14:01:16 -0700

    style

--- app/constants.ts
+++ app/constants.ts
@@ -45,9 +45,9 @@ export const OPTION_TO_NAME_DEFAULT_VALUES: {[key: string]: string} = {
 
 // default price metadata values
 export const PRODUCT_METADATA_DEFAULT_VALUES: ProductMetadata = {
-    "sizeToPrice": SIZE_TO_PRICE_DEFAULT_VALUES,
-    "flowerToPrice": FLOWER_TO_PRICE_DEFAULT_VALUES,
-    "optionToName": OPTION_TO_NAME_DEFAULT_VALUES
+    sizeToPrice: SIZE_TO_PRICE_DEFAULT_VALUES,
+    flowerToPrice: FLOWER_TO_PRICE_DEFAULT_VALUES,
+    optionToName: OPTION_TO_NAME_DEFAULT_VALUES
 };
 
 export const PRODUCT_METADATA_DEFAULT_VALUES_SERIALIZED = JSON.stringify(PRODUCT_METADATA_DEFAULT_VALUES);
